c3ec687a91ef32e1c51c26ea38f46fa31dddef4e
Kinasiykwim <dovydasb28@gmail.com>
2026-03-06 18:10:46 +0200

    Login puslapy prideti email ir password laukai

--- Frontend/obj/Debug/net10.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net10.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e5e53b2c682df6e62eeb7f8cd6428d88fbb0ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592464b11b72011a2329fb50a1ced21ecc2514dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
